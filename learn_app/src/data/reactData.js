793cdc215c4542041defdeabbc3d9c6b77813faf
nicolay <sirghiinicolai1@gmail.com>
2023-10-06 12:50:48 +0200

    some more

--- learn_app/src/data/reactData.js
+++ learn_app/src/data/reactData.js
@@ -1,12 +1,100 @@
 export const reactArray = [
-
+    
     {
+            "id": 18,
+            "addingId": 10,
+            "dataToday": "Fri Oct 06 2023 12:36:55",
+            "title": "React",
+            "question": "How to import the configureStore from \"redux-toolkit\"?",
+            "answer": "import { configureStore } from \"@reduxjs/toolkit\";",
+            "ourAnswer": "",
+            "fakeAnswer": "",
+            "image": "url(https://i.ytimg.com/vi/bTf1TWVBBOk/maxresdefault.jpg)"
+        },
+        {
+            "id": 17,
+            "addingId": 10,
+            "dataToday": "Fri Oct 06 2023 12:33:56",
+            "title": "React",
+            "question": "How to install redux-toolkit as a dependency?",
+            "answer": "npm install @reduxjs/toolkit",
+            "ourAnswer": "",
+            "fakeAnswer": "",
+            "image": "url(https://hybridheroes.de/blog/content/images/2022/03/redux-toolkit-1400.jpg)"
+        },
+        {
+            "id": 16,
+            "addingId": 10,
+            "dataToday": "Fri Oct 06 2023 12:29:59",
+            "title": "React",
+            "question": "what is main difference between redux and redux toolkit?",
+            "answer": "a lot lesser boilerplate code is required compared to Redux",
+            "ourAnswer": "",
+            "fakeAnswer": "",
+            "image": "url(https://media.licdn.com/dms/image/D5612AQFcwVrtpQKTUQ/article-cover_image-shrink_720_1280/0/1688295760381?e=2147483647&v=beta&t=XBJLJyCx1IjfTfvfCpEDRvNwMC603oc95IKOIX-YqXY)"
+        },
+        {
+            "id": 15,
+            "addingId": 9,
+            "dataToday": "Fri Oct 06 2023 11:56:50",
+            "title": "React",
+            "question": "How do we declare the \"Provider\" from \"react-redux\"?",
+            "answer": "<Provider store={store}><App /></Provider>",
+            "ourAnswer": "",
+            "fakeAnswer": "",
+            "image": "url(https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/content/cover_image_file/cover_image/1279225/retina_1708x683_0521-react-redux-and-immutablejs-Waldek_Newsletter-993b50f4ae56e9ee6e024a309c23a6c4.png)"
+        },
+        {
+            "id": 14,
+            "addingId": 9,
+            "dataToday": "Fri Oct 06 2023 11:53:24",
+            "title": "React",
+            "question": "In which component do you import the \"Provider\" from \"react-redux\"?",
+            "answer": "index.js",
+            "ourAnswer": "",
+            "fakeAnswer": "",
+            "image": "url(https://www.bairesdev.com/wp-content/uploads/2022/06/Picture6-1.svg)"
+        },
+        {
+            "id": 13,
+            "addingId": 9,
+            "dataToday": "Fri Oct 06 2023 11:51:20",
+            "title": "React",
+            "question": "How do you import the  <Provider /> component from \"react-redux\"?",
+            "answer": "import { Provider } from 'react-redux';",
+            "ourAnswer": "",
+            "fakeAnswer": "",
+            "image": "url(https://miro.medium.com/v2/resize:fit:1200/0*jiAWxJpDhUAGXjzx)"
+        },
+        {
+            "id": 12,
+            "addingId": 9,
+            "dataToday": "Fri Oct 06 2023 11:48:14",
+            "title": "React",
+            "question": "What does the <Provider /> component from \"react-redux do?",
+            "answer": "it makes the Redux store available to the rest of your app",
+            "ourAnswer": "",
+            "fakeAnswer": "",
+            "image": "url(https://i.ytimg.com/vi/ZRE6e_0eQeQ/maxresdefault.jpg)"
+        },
+        {
+            "id": 11,
+            "addingId": 9,
+            "dataToday": "Fri Oct 06 2023 11:45:21",
+            "title": "React",
+            "question": "How to  install \"react-redux\" as a dependency?",
+            "answer": "npm install react-redux",
+            "ourAnswer": "",
+            "fakeAnswer": "",
+            "image": "url(https://i.ytimg.com/vi/DiLVAXlVYR0/maxresdefault.jpg)"
+        },
+        {
             "id": 10,
             "addingId": 8,
             "dataToday": "Thu Sep 28 2023 16:06:54",
             "title": "React",
             "question": "What is redux?",
-            "answer": "JavaScript library designed to manage the state of data in JavaScript applications.",
+            "answer": "JavaScript library designed to manage the state of data in JavaScript applications",
             "ourAnswer": "",
             "fakeAnswer": "",
             "image": "url(https://redux.js.org/img/redux-logo-landscape.png)"
@@ -17,7 +105,7 @@ export const reactArray = [
             "dataToday": "Thu Sep 28 2023 16:04:17",
             "title": "React",
             "question": "what is react-redux?",
-            "answer": "the official React binding for Redux.",
+            "answer": "the official React binding for Redux",
             "ourAnswer": "",
             "fakeAnswer": "",
             "image": "url(https://www.esri.com/arcgis-blog/wp-content/uploads/2017/09/react-redux-overview.png)"
@@ -50,7 +138,7 @@ export const reactArray = [
             "dataToday": "Thu Sep 28 2023 15:51:31",
             "title": "React",
             "question": "What does useEffect do?",
-            "answer": "you tell React that your component needs to do something after render.",
+            "answer": "you tell React that your component needs to do something after render",
             "ourAnswer": "",
             "fakeAnswer": "",
             "image": "url(https://daveceddia.com/images/useEffect-hook.png)"
@@ -61,7 +149,7 @@ export const reactArray = [
             "dataToday": "Thu Sep 28 2023 14:04:04",
             "title": "React",
             "question": "What is \"useMemo\"?",
-            "answer": "Hook that lets you cache the result of a calculation between re-renders.",
+            "answer": "hook that lets you cache the result of a calculation between re-renders",
             "ourAnswer": "",
             "fakeAnswer": "",
             "image": "url(https://blog.alexdevero.com/wp-content/uploads/2021/05/31-05-21-the-react-usememo-hook-made-simple-blog.jpg)"
